f007056423c545ed34b3b5b3bfbdbd067956be5f
Kate Burgess <kateburgess@dyn221077.cc.lehigh.edu>
2026-02-25 09:38:47 -0500

    Basic flutter app w camera

---
+++ android/app/src/main/kotlin/com/example/my_low_vision_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_low_vision_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+//code tutorial: https://docs.flutter.dev/cookbook/plugins/picture-using-camera 
+
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TakePictureScreen(),
+    );
+  }
+}
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({super.key});
+
+  @override
+  State<TakePictureScreen> createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      cameras.first,
+      ResolutionPreset.high,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a Picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+
+            final image = await _controller.takePicture();
+
+            final directory = await getApplicationDocumentsDirectory();
+            final name = basename(image.path);
+            final imagePath = '${directory.path}/$name';
+
+            await image.saveTo(imagePath);
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Picture saved: $imagePath')),
+            );
+          } catch (e) {
+            debugPrint(e.toString());
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
